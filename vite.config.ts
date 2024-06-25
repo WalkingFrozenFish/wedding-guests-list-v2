bdae4dbf9520138e119ab6ec4f173f9cb208c535
Oleg Chachin <chachin.ol@yandex.kz>
2024-06-25 13:55:55 +0500

    Fix path

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/wedding-guests-list-s/"
+  base: "/wedding-guests-list-v2/"
 })
